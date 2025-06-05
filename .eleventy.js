75830ac09b159f2926014595732c45a3b92fe09a
Kangping <69379510+kangping-git@users.noreply.github.com>
2025-06-05 20:50:39 +0900

    Fix template filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,25 @@
+const { DateTime } = require('luxon');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     'src/assets/images': 'assets/images',
     'src/assets/js': 'assets/js'
+  });
+
+  eleventyConfig.addFilter('filterByTag', (posts, tag) => {
+    return (posts || []).filter(post => Array.isArray(post.data.tags) && post.data.tags.includes(tag));
+  });
+
+  eleventyConfig.addFilter('filterByCategory', (posts, category) => {
+    return (posts || []).filter(post => Array.isArray(post.data.categories) && post.data.categories.includes(category));
+  });
+
+  eleventyConfig.addFilter('date', (dateObj, format = 'yyyy-MM-dd') => {
+    if(!dateObj) {
+      return '';
+    }
+    let dt = (dateObj instanceof Date) ? DateTime.fromJSDate(dateObj) : DateTime.fromISO(dateObj.toString());
+    return dt.toFormat(format);
   });
 
   return {
